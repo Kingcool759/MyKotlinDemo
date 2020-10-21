04b3069f91411cfdc868822e720cdb44144a780a
zhaojianwei01 <3058587756@qq.com>
2020-10-21 19:53:23 +0800

    02Recycler代码简化

--- app/src/main/java/com/example/mykotlindemo/display/Kotlin02.kt
+++ app/src/main/java/com/example/mykotlindemo/display/Kotlin02.kt
@@ -11,30 +11,17 @@ import kotlinx.android.synthetic.main.activity_kotlin02.*
 
 @Route(path = RouterPath.kotlin02)
 class Kotlin02 : AppCompatActivity() {
-    private val textList = ArrayList<String>()
+    private val textList = arrayListOf("我的关注","通知开关", "我的徽章", "意见反馈", "我要投稿",
+        "我的关注","通知开关", "我的徽章", "意见反馈", "我要投稿",
+        "我的关注","通知开关","我的徽章","意见反馈","我要投稿")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin02)
         //使用Recycler
-        initTextList()
         val layoutManager = LinearLayoutManager(this)
         mRecycler.layoutManager = layoutManager
         val adapter = RecyclerAdapter(textList)
         mRecycler.adapter = adapter
-    }
-    private fun initTextList(){
-        textList.add("我的关注")
-        textList.add("观察记录")
-        textList.add("通知开关")
-        textList.add("我的徽章")
-        textList.add("意见反馈")
-        textList.add("我要投稿")
-        textList.add("我的关注")
-        textList.add("观察记录")
-        textList.add("通知开关")
-        textList.add("我的徽章")
-        textList.add("意见反馈")
-        textList.add("我要投稿")
     }
 }
