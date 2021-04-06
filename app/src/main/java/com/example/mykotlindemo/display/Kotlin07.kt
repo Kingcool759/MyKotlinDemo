2754f8342117ebffe7117e97ad8137ff2edbbf30
armStrong759 <3058587756@qq.com>
2021-04-06 10:02:46 +0800

    Glide

--- app/src/main/java/com/example/mykotlindemo/display/Kotlin07.kt
+++ app/src/main/java/com/example/mykotlindemo/display/Kotlin07.kt
@@ -3,8 +3,11 @@ package com.example.mykotlindemo.display
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.bumptech.glide.Glide
 import com.example.mykotlindemo.R
 import com.example.mykotlindemo.arout.RouterPath
+import com.example.mykotlindemo.glide.NetCacheUtils
+import kotlinx.android.synthetic.main.activity_kotlin07.*
 
 //Glide三级缓存实战
 @Route(path = RouterPath.kotlin07)
@@ -12,5 +15,9 @@ class Kotlin07 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin07)
+
+        Glide.with(this)
+            .load("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3405350801,859040723&fm=26&gp=0.jpg")
+            .into(iVImageViewg)
     }
 }
